42596c7dc3560bf169217bdddae45c5adae50d0b
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-29 00:43:40 +0530

    using json server

--- src/home.js
+++ src/home.js
@@ -7,22 +7,18 @@ const Home = () => {
         { title: 'my new web33333', body: 'lorem 333333 ipsum ...', author: "mario", id: 3 }
     ]);
 
-    const [name , setName] = useState("mario");
-
     const HandelDelete = (id) =>{
         const NewBlog = blogs.filter(blog => blog.id !== id);
         setBlogs(NewBlog);
     }
 
     useEffect(() => {
-        console.log("useEffect run now onley when dipendency [] ke andar ka useState re-render hoga   in this case only when name changes and not when blogs changes   allso it allwes work when we start the web  ");
-        console.log(name);
-    } ,[name] );
+        console.log("useEffect run ");
+    } ,[] );
 
     return (
         <div className="home">
             <Bloglist blogs = {blogs} title ="all bloags" HandelDelete= {HandelDelete} />
-            <button onClick={() => setName("sumit")} >change name </button>
         </div>
     );
 }
