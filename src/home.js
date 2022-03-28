5d079b6b7498fc86092febdcb53f6ca72ab48c81
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-29 01:58:34 +0530

    catching errors

--- src/home.js
+++ src/home.js
@@ -4,24 +4,36 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] =useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/blogs")
                 .then(res => {
+                    if (!res.ok){
+                        throw Error("could not featch the data for theat reasorce")
+
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setLoading(false);
+                    setError(null);
                 })
-        }, 2000);
+                .catch(err =>{
+                    setError(err.message)
+                    setLoading(false);
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {loading && <div> loading...... </div>}
             {blogs && <Bloglist blogs={blogs} title="all bloags" />}
+            
         </div>
     );
 }
