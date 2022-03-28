edaa0e8244e8e3078d8f05c9e576d76264e101bb
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-29 01:34:03 +0530

    conditional loading masseag

--- src/home.js
+++ src/home.js
@@ -1,22 +1,27 @@
 import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
 const Home = () => {
-    
+
     const [blogs, setBlogs] = useState(null);
-   
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => {
-           return res.json();
-        })
-        .then(data =>{
-            setBlogs(data);
-        })
-    } ,[] );
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setLoading(false);
+                })
+        }, 2000);
+    }, []);
 
     return (
         <div className="home">
-            { blogs && <Bloglist blogs = {blogs} title ="all bloags"  /> }
+            {loading && <div> loading...... </div>}
+            {blogs && <Bloglist blogs={blogs} title="all bloags" />}
         </div>
     );
 }
