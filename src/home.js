d7ce946d3f8a9b771c40432a078e14bcc7144d77
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-28 23:38:36 +0530

    filtering an object and reusing an component

--- src/home.js
+++ src/home.js
@@ -4,12 +4,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'my new web', body: 'lorem ipsum ...', author: "mario", id: 1 },
         { title: 'my new web 2222', body: 'lorem 22222 ipsum ...', author: "mario2222", id: 2 },
-        { title: 'my new web33333', body: 'lorem 333333 ipsum ...', author: "mario3333", id: 3 }
+        { title: 'my new web33333', body: 'lorem 333333 ipsum ...', author: "mario", id: 3 }
     ]);
 
     return (
         <div className="home">
             <Bloglist blogs = {blogs} title ="all bloags"/>
+            <Bloglist blogs = {blogs.filter((blog) => blog.author === "mario")} title ="marios blog"/>
         </div>
     );
 }
