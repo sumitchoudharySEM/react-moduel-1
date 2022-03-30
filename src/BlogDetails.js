03c503e7e77ec458f5eb2a091d213eaf95ac45ba
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-30 14:31:51 +0530

    deleteing blog using fetch delete method

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { error, loading, data: blog } = useFetch(` http://localhost:8000/blogs/` + id);
+    const { error, loading, data: blog } = useFetch(`http://localhost:8000/blogs/` + id);
+    const history = useHistory();
+    const handleDelete = () =>{
+        fetch(`http://localhost:8000/blogs/`+ blog.id ,{
+            method : "DELETE"
+        }).then(() =>{
+            history.push("/");
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -14,6 +22,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <h3>{blog.author}</h3>
                     <p>{blog.body}</p>
+                    <button onClick={handleDelete}>delete</button>
                 </article>}
         </div>
     );
