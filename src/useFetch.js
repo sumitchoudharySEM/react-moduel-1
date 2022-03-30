6ce91e2adeb01c94bccbec85a6425fb5e22dbf0a
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-30 11:30:58 +0530

    useEffect Cleanup by abort controler

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,13 +4,15 @@ const useFetch = (url) => {
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [error, setError] =useState(null);
-    
+    const [error, setError] = useState(null);
+
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
-                    if (!res.ok){
+                    if (!res.ok) {
                         throw Error("could not featch the data for theat reasorce")
                     }
                     return res.json();
@@ -20,14 +22,21 @@ const useFetch = (url) => {
                     setLoading(false);
                     setError(null);
                 })
-                .catch(err =>{
-                    setError(err.message)
-                    setLoading(false);
+                .catch(err => {
+                    if (err.name === "AbortError") {
+                        console.log(" featch aborted")
+                    }
+                    else {
+                        setError(err.message)
+                        setLoading(false);
+                    }
                 })
         }, 1000);
+        return () => abortCont.abort();
+
     }, [url]);
 
     return { data, loading, error };
 }
- 
+
 export default useFetch;
