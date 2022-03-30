a53a5e9bfb8b4877f30ba82b97283577b8264cff
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-03-30 13:02:49 +0530

    Reusing Custom Hooks useFech.js in bloagdeatiles.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const { error, loading, data: blog } = useFetch(` http://localhost:8000/blogs/` + id);
 
     return (
         <div className="blog-details">
-            <h2>
-                see Blog details heair - {id}
-            </h2>
+            {error && <div>{error}</div>}
+            {loading && <div>Loading...</div>}
+            {blog &&
+                <article>
+                    <h2>{blog.title}</h2>
+                    <h3>{blog.author}</h3>
+                    <p>{blog.body}</p>
+                </article>}
         </div>
     );
 }
